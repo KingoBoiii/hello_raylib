23a1fc59ae0753ca8e9075e2f9b5bc8c3d1046e7
KingoBoiii <kingodk@outlook.dk>
2023-09-14 13:17:59 +0200

    Added raylib and raygui

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <vk.h>
+#include <raylib.h>
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
 
 int main() {
     print("Hello, raylib (+ raygui)!");
+
+    InitWindow(800, 450, "raylib [core] example - basic window");
+
+    int listViewScrollIndex = 0;
+    int listViewActive = -1;
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        GuiListView((Rectangle){ 165, 25, 140, 124 }, "Charmander;Bulbasaur;#18#Squirtel;Pikachu;Eevee;Pidgey", &listViewScrollIndex, &listViewActive);
+
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
     return 0;
 }
